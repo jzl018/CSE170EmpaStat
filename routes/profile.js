3cc9a04ab585d458e301785f8587bc608fd5aaac
jzl018@ucsd.edu <jzl018@ucsd.edu>
2017-03-07 14:04:08 -0800

    attempt 2 at google analytics a/b testing

--- routes/profile.js
+++ routes/profile.js
@@ -2,11 +2,13 @@
 var data = require('../data.json');
 
 exports.viewProfile = function(req, res){
-	data["side"] = true;
+	data["side"] = req.session.side;
+	console.log(req.session.side)
 	res.render('profile', data);
 };
 
-exports.viewProfileBottom = function(req, res){
+/*exports.viewProfileBottom = function(req, res){
 	data["side"] = false;
 	res.render('profile', data);
 };
+*/
